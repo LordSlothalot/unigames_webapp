e62c560901a86f1f60f4e428ca8910cb65d80307
Luke Stagoll <lukestagoll16@gmail.com>
2020-08-21 16:25:39 +0800

    moved user model to separate file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
 
-
 #app.config dictionary is a general-purpose place to store config variables
 #The SECRET_KEY is needed for loading flask-wtf forms. 
 app.config['SECRET_KEY'] = 'this should be a password but whatever'
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/unigames_webapp_db"
+
+mongo = PyMongo(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,96 +1,10 @@
-from app import app
-from flask import Flask, render_template, url_for, redirect, request, flash, session
+from app import app, mongo, login
+from flask import Flask, render_template, url_for, redirect, request, flash
 from app.forms import LoginForm, RegistrationForm
+from app.user_models import User
 from flask_pymongo import PyMongo
-from flask_login import LoginManager, current_user, login_user, logout_user, login_required
-from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.urls import url_parse
-from uuid import uuid4
-
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/unigames_webapp_db"
-
-mongo = PyMongo(app)
-login = LoginManager(app)
-login.login_view = 'login'
-
-class User:
-    def __init__(self, email, password, first_name, last_name, _id=None):
-        self.email = email
-        self.password = password
-        self._id = uuid4().hex if _id is None else _id
-        self.first_name = first_name
-        self.last_name = last_name
-    
-    @staticmethod
-    def is_authenticated():
-        return True
-
-    @staticmethod
-    def is_active():
-        return True
-
-    @staticmethod
-    def is_anonymous():
-        return False
-
-    def get_id(self):
-        return self._id
-
-    @staticmethod
-    def check_password(password_hash, password):
-        return check_password_hash(password_hash, password)
-
-    @classmethod
-    def get_by_email(cls, email):
-        data = mongo.db.Users.find_one({"email": email})
-        if data is not None:
-            return cls(**data)
-
-    @classmethod
-    def get_by_id(cls, _id):
-        data = mongo.db.Users.find_one({"_id": _id})
-        if data is not None:
-            return cls(**data)
-
-    @staticmethod
-    def login_valid(email, password):
-        verify_user = User.get_by_email(email)
-        if verify_user is not None:
-            return check_password_hash(verify_user.password, password)
-        return False
-
-    @classmethod
-    def register(cls, email, password, first_name, last_name):
-        user = cls.get_by_email(email)
-        if user is None:
-            new_user = cls(email, password, first_name, last_name)
-            new_user.save_to_mongo()
-            session['email'] = email
-            return True
-        else:
-            return False
-
-    def json(self):
-        return {
-            "_id": self._id,
-            "email": self.email,
-            "password": self.password,
-            "first_name": self.first_name,
-            "last_name": self.last_name
-        }
-
-    def save_to_mongo(self):
-        mongo.db.Users.insert(self.json())
-
-    @login.user_loader
-    def load_user(email):
-        u = mongo.db.Users.find_one({'email': email})
-        if not u:
-            return None
-        return User(email=u['email'])
-
-
+from flask_login import LoginManager, current_user, login_user, logout_user
+from werkzeug.security import generate_password_hash
 
 @app.route('/')
 @app.route('/index')
