7e52599211aef77364dde4ab46d87f18c9c415a2
Luke Stagoll <lukestagoll16@gmail.com>
2020-10-16 12:29:24 +0800

    Added documentation for roles.py

--- backend/app/database_impl/roles.py
+++ backend/app/database_impl/roles.py
@@ -7,19 +7,54 @@ from flask_pymongo import PyMongo
 
 
 class Permissions(Enum):
+    """
+    A class for permissions users hold
+    """
+
     CanEditItems = "can_edit_items"
+    """
+    Users with this permission can edit items
+    """
+
     CanEditUsers = "can_edit_users"
+    """
+    Users with this permission can edit other users
+    """
+    
     CanViewHidden = "can_view_hidden"
+    """
+    Users with this permission can view hidden items
+    """
+    
 
     def __str__(self):
         return self.value
 
 
 class Role:
+    """
+    A class to represent roles of users
+    """
+
     id: ObjectId
+    """
+    Role id
+    """
+
     name: str
+    """
+    Role name
+    """
+
     priority: int
+    """
+    Role priority. A high number is high priority
+    """
+
     permissions: Dict[Permissions, bool]
+    """
+    A dictionary containing role permissions.
+    """
 
     @staticmethod
     def init_indices(mongo: PyMongo):
@@ -32,6 +67,13 @@ class Role:
         self.permissions = permissions
 
     def to_dict(self) -> Dict:
+        """
+	    Serialising the data structure into a MongoDB compliant dictionary for use in PyMongo functions
+
+        Returns
+        -------
+            The MongoDB compliant data structure
+        """
         result = {
             "name": self.name,
             "priority": self.priority,
@@ -45,6 +87,18 @@ class Role:
 
     @staticmethod
     def from_dict(value_dict: Dict) -> 'Role':
+        """
+	    Deserialising a MongoDB compliant dictionary into a data structure for use in python functions
+
+        Parameters
+        ----------
+            value_dict
+                The dictionary to be deserialised
+
+        Returns
+        -------
+            The deserialised data structure
+        """
         cls = Role("", 0, {})
 
         if "_id" in value_dict:
@@ -64,6 +118,17 @@ class Role:
         return cls
 
     def write_to_db(self, mongo: PyMongo):
+        """
+        Writes the role to the database if it's `id` doesn't exist, 
+        otherwise it will overwrite the role with the same `id`
+
+        Attributes
+        ----------
+            self
+                the role to be updated
+            mongo
+                the database
+        """
         if self.id is None:
             self.id = mongo.db.roles.insert_one(self.to_dict()).inserted_id
         else:
@@ -71,6 +136,21 @@ class Role:
 
     # Returns True if the update worked, else False, usually meaning it's no longer there
     def update_from_db(self, mongo: PyMongo) -> bool:
+        """
+        Updates a role in the database
+
+        Attributes
+        ----------
+            self
+                the role to be updated
+            mongo
+                the database
+
+        Returns
+        -------
+            False if the role's id is None
+            False if the role does not exist in the database
+        """
         if self.id is None:
             return False
 
@@ -86,6 +166,21 @@ class Role:
         self.permissions = new_role.permissions
 
     def delete_from_db(self, mongo: PyMongo) -> bool:
+        """
+        Removes a role from the database
+
+        Attributes
+        ----------
+            self
+                the role to be deleted
+            mongo
+                the database
+
+        Returns
+        -------
+            False if the role does not exist, or has not been deleted
+            True if the the role has been deleted
+        """
         if self.id is None:
             return False
 
@@ -93,6 +188,21 @@ class Role:
 
     @staticmethod
     def search_for_by_name(mongo: PyMongo, name: str) -> Optional['Role']:
+        """
+        Finds a role in the database with the specified name
+
+        Attributes
+        ----------
+            mongo
+                the database
+            tag_ref
+                the name to be searched by
+
+        Returns
+        -------
+            None if a role has not been found
+            The role if it exists in the database
+        """
         result = mongo.db.roles.find_one({"name": name})
         if result is None:
             return None
@@ -101,6 +211,28 @@ class Role:
     
     @classmethod
     def create_new(cls, mongo: PyMongo, name: str, priority: int, can_view_hidden: bool, can_edit_users: bool, can_edit_items: bool):
+        """
+        Create a new role and inserts it into the database
+
+        Attributes
+        ----------
+            mongo
+                the database
+            name
+                the name of the role
+            priority
+                the role priority
+            can_view_hidden
+                Users with this role can view hidden items
+            can_edit_users
+                Users with this role can edit other users
+            can_edit_items
+                Users with this role can edit items
+
+        Returns
+        -------
+            True if the role has been created, else false
+        """
         role = cls.search_for_by_name(mongo, name)
         if role is None:
             permissions = {}
