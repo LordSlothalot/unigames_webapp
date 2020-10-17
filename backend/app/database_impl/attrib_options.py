133785e972f40f7f9d69c12c01a6468d365db2da
Luke Stagoll <lukestagoll16@gmail.com>
2020-10-17 12:33:14 +0800

    Added docs for attrib_options.py

--- backend/app/database_impl/attrib_options.py
+++ backend/app/database_impl/attrib_options.py
@@ -8,6 +8,9 @@ from flask_pymongo import PyMongo
 
 
 class AttributeTypes(IntEnum):
+    """
+    Enum class for attibute types
+    """
     Invalid = 0
     SingleLineString = 1
     MultiLineString = 2
@@ -17,7 +20,15 @@ class AttributeTypes(IntEnum):
 
 
 class Attribute(metaclass=abc.ABCMeta):
+    """
+    A class for item attibutes
+    """
+
     attrib_type: AttributeTypes
+    """
+    The type of attribute
+    """
+
     option_id: ObjectId
     value = None
 
@@ -33,7 +44,18 @@ class Attribute(metaclass=abc.ABCMeta):
 
     @staticmethod
     def from_dict(dict_value: Dict) -> 'Attribute':
+        """
+	    Deserialising a MongoDB compliant dictionary into a data structure for use in python functions
 
+        Parameters
+        ----------
+            value_dict
+                The dictionary to be deserialised
+
+        Returns
+        -------
+            The deserialised data structure
+        """
         if "option_id" not in dict_value or dict_value["option_id"] is None:
             raise ValueError("Requires an option_id field")
         option_id = dict_value["option_id"]
@@ -57,50 +79,116 @@ class Attribute(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def to_dict(self) -> Dict:
+        """
+	    Serialising the data structure into a MongoDB compliant dictionary for use in PyMongo functions
+
+        Returns
+        -------
+            The MongoDB compliant data structure
+        """
         return {"option_id": self.option_id, "attrib_type": self.attrib_type}
 
 
 class SingleLineStringAttribute(Attribute):
+    """
+    A class to represent single line string attributes
+    """
+
     def __init__(self, option_id: Union['AttributeOption', ObjectId], text: str):
         super().__init__(option_id, AttributeTypes.SingleLineString)
         self.value = text
 
     def to_dict(self) -> Dict:
+        """
+	    Serialising the data structure into a MongoDB compliant dictionary for use in PyMongo functions
+
+        Returns
+        -------
+            The MongoDB compliant data structure
+        """
         return {**super().to_dict(), "value": self.value}
 
 
 class MultiLineStringAttribute(Attribute):
+    """
+    A class to represent multi line string attributes
+    """
+
     def __init__(self, option_id: Union['AttributeOption', ObjectId], text: List[str]):
         super().__init__(option_id, AttributeTypes.MultiLineString)
         self.value = text
 
     def to_dict(self) -> Dict:
+        """
+	    Serialising the data structure into a MongoDB compliant dictionary for use in PyMongo functions
+
+        Returns
+        -------
+            The MongoDB compliant data structure
+        """
         return {**super().to_dict(), "value": self.value}
 
 
 class SingleLineIntegerAttribute(Attribute):
+    """
+    A class to represent single line integer attributes
+    """
+
     def __init__(self, option_id: Union['AttributeOption', ObjectId], val: int):
         super().__init__(option_id, AttributeTypes.SingleLineInteger)
         self.value = val
 
     def to_dict(self) -> Dict:
+        """
+	    Serialising the data structure into a MongoDB compliant dictionary for use in PyMongo functions
+
+        Returns
+        -------
+            The MongoDB compliant data structure
+        """
         return {**super().to_dict(), "value": self.value}
 
 
 class PictureAttribute(Attribute):
+    """
+    A class to represent a picture attribute
+    """
+
     def __init__(self, option_id: Union['AttributeOption', ObjectId], picture_file_id: ObjectId):
         super().__init__(option_id, AttributeTypes.Picture)
         self.value = picture_file_id
 
     def to_dict(self) -> Dict:
+        """
+	    Serialising the data structure into a MongoDB compliant dictionary for use in PyMongo functions
+
+        Returns
+        -------
+            The MongoDB compliant data structure
+        """
         return {**super().to_dict(), "value": self.value}
 
 
 class AttributeOption:
     id: ObjectId = None
+    """
+    The attribute id
+    """
+
     hidden: bool = False
+    """
+    Hides the attribute if True
+    """
+
     attribute_name: str = None
+    """
+    The name of the attribute
+    """
+
     attribute_type: AttributeTypes = AttributeTypes.Invalid
+    """
+    The type of attribute
+    """
 
     @staticmethod
     def init_indices(mongo: PyMongo):
@@ -114,6 +202,18 @@ class AttributeOption:
 
     @staticmethod
     def from_dict(value_dict: Dict) -> 'AttributeOption':
+        """
+	    Deserialising a MongoDB compliant dictionary into a data structure for use in python functions
+
+        Parameters
+        ----------
+            value_dict
+                The dictionary to be deserialised
+
+        Returns
+        -------
+            The deserialised data structure
+        """
         cls = AttributeOption("", AttributeTypes.Invalid)
 
         if "attribute_name" not in value_dict or value_dict["attribute_name"] is None:
@@ -130,6 +230,13 @@ class AttributeOption:
         return cls
 
     def to_dict(self) -> Dict:
+        """
+	    Serialising the data structure into a MongoDB compliant dictionary for use in PyMongo functions
+
+        Returns
+        -------
+            The MongoDB compliant data structure
+        """
         result = {
             "attribute_name": self.attribute_name.lower(),
             "attribute_type": int(self.attribute_type)
@@ -144,6 +251,15 @@ class AttributeOption:
         return result
 
     def write_to_db(self, mongo: PyMongo):
+        """
+        Writes the attribute to the database if it's `id` doesn't exist, 
+        otherwise it will overwrite the attribute with the same `id`
+
+        Parameters
+        ----------
+            mongo
+                the mongo database
+        """
         if self.id is None:
             self.id = mongo.db.attrib_options.insert_one(self.to_dict()).inserted_id
         else:
@@ -151,6 +267,19 @@ class AttributeOption:
 
     # Returns True if the update worked, else False, usually meaning it's no longer there
     def update_from_db(self, mongo: PyMongo) -> bool:
+        """
+        Updates the attribute in the database
+
+        Parameters
+        ----------
+            mongo
+                The mongo database
+
+        Returns
+        -------
+            False if the attribute's id is None
+            False if the attribute does not exist in the database
+        """
         if self.id is None:
             return False
 
@@ -166,6 +295,19 @@ class AttributeOption:
         self.attribute_name = new_item_attribute.attribute_name
 
     def delete_from_db(self, mongo: PyMongo) -> bool:
+        """
+        Removes the attribute from the database
+
+        Parameters
+        ----------
+            mongo
+                The database
+
+        Returns
+        -------
+            False if the attribute does not exist, or has not been deleted
+            True if the the attribute has been deleted
+        """
         if self.id is None:
             return False
 
@@ -173,6 +315,21 @@ class AttributeOption:
 
     @staticmethod
     def search_for_by_name(mongo: PyMongo, attribute_name: str) -> Optional['AttributeOption']:
+        """
+        Finds an attribute in the database with the specified name
+
+        Parameters
+        ----------
+            mongo
+                The mongo database
+            name
+                The name to be searched by
+
+        Returns
+        -------
+            None if an attribute has not been found
+            The attribute if it exists in the database
+        """
         result = mongo.db.attrib_options.find_one({"attribute_name": attribute_name.lower()})
         if result is None:
             return None
